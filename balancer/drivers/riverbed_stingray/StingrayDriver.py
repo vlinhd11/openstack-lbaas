cc5ecff6aaac29ab9d5e80697b06e8f1f8c12163
Harry Ragan <harry.ragan@riverbed.com>
2012-09-03 14:25:13 +0100

    First support for virtual IPs added. Major code cleanup.

--- balancer/drivers/riverbed_stingray/StingrayDriver.py
+++ balancer/drivers/riverbed_stingray/StingrayDriver.py
@@ -6,23 +6,29 @@ import urlparse
 import json
 import requests
 from requests.auth import HTTPBasicAuth
+from requests.exceptions import HTTPError
 
 from balancer.drivers.base_driver import BaseDriver
 
 logger = logging.getLogger(__name__)
 
 class StingrayDriver(BaseDriver):
+    ''' Most (all)  parameters can be found in db.models.py. They all inherit from
+    a class which implements the functions allowing for dictionary notation.
+    Using this notation allows for easy unit testing.
+    '''
     def __init__(self,  conf,  device_ref):
         super(StingrayDriver, self).__init__(conf, device_ref)
-        #Store
+        #Store id and name for later functions
         self.device_id = device_ref['id']
+        self.device_name = device_ref['name']
 
         #Standard port for REST daemon is 9070
         port = '9070'
         #Overwrite default port if one is specified
         try:
             if device_ref['port'] is not None:
-                port = '9070'
+                port = device_ref['port']
         except KeyError:
             pass
 
@@ -32,15 +38,14 @@ class StingrayDriver(BaseDriver):
         self.basic_auth = HTTPBasicAuth(device_ref['user'],
                             device_ref['password'])
 
-
-    def __del__(self):
-        response = self.send_request('vservers/' + self.name, 'DELETE')
-
     def send_request(self, url_extension, method, payload=None):
+        ''' Wrapper around the python requests library. Ensures that valiidity
+        of the SSL certificate is ignored and that requests include HTTP Basic Auth.
+        '''
         #Generate appropriate url
         target_url = urlparse.urljoin(self.url, url_extension)
         #Create headers dictionary
-        #FIXME: Check If-Match with someone on the REST team
+        #If-Match not implemented by REST team as of yet 
         headers = {'Content-Type': 'application/json',
                     'Accept': 'application/json',
                     'If-Match': 'NEW'}
@@ -49,7 +54,7 @@ class StingrayDriver(BaseDriver):
                         + ':' + str(payload))
         try:
             #Send request
-            #Stingray API on wiki explains what mehtod is approprite
+            #Stingray API on wiki explains what method is approprite
             if method == 'GET':
                 response = requests.get(target_url, headers=headers,
                                 auth=self.basic_auth, verify=False)
@@ -61,20 +66,70 @@ class StingrayDriver(BaseDriver):
                 response = requests.post(target_url, headers=headers,
                                 data=json.dumps(payload),
                                 auth=self.basic_auth, verify=False)
-
             elif method == 'DELETE':
                 response = requests.delete(target_url, headers=headers,
                                 auth=self.basic_auth, verify=False)
-
         except Exception:
-            #Most likely could not reach the specified URL
+            '''Most likely could not reach the specified URL.
+            Useful to let errors be passed on to be dealt with in
+            calling function
+            '''
             raise
 
         logger.debug("Data from Stingray:\n" + response.text)
-        #Make sure error code is acceptable, ensures unit tests will fail
+        #Make sure request reported as successful.
         response.raise_for_status()
 
         return response
+
+    def rest_add_to_list(self, target, field_name,
+                            item_to_add, dict_to_add_to):
+        ''' Given a target url, property name and dictionary add the new item
+        to the list stored in field and return the new list in the correct
+        Stingray REST format. Can be used regardless of whether field currently
+        exists.
+        '''
+        #TODO: Get new version of REST so list syntax used.
+        #Requests data from Stingray and extract current list 
+        current_json = self.send_request(target, 'GET').text
+        current = json.loads(current_json)
+
+        try:
+            old_list = current['properties'][field_name]
+        except KeyError:
+            old_list = ''
+
+        #Stored as space seperated variable string.
+        new_list = old_list + ' '  + item_to_add
+
+        #Add new node to list and put in correct dictionary form
+        dict_to_add_to['properties'][field_name] = new_list
+
+        return dict_to_add_to
+
+    def rest_delete_from_list(self, target, field_name,
+                                item_to_remove, dict_to_remove_from):
+        ''' Given a target url, property name and dictionary removes the item
+        from the list stroed in field and returns the list in the correct
+        Stingray REST format. If not found just returns dictionary with empty
+        list.
+        '''
+        #??change to throw item not found exception??
+        #TODO: Get new version of REST so list syntax used.
+        current_json = self.send_request(target, 'GET').text
+        current = json.loads(current_json)
+
+        try:
+            old_list = current['properties'][field_name]
+        except KeyError:
+            #Raise no item found exception?
+            old_list = ''
+
+        #remove item and replace list in dictionary
+        new_list = old_list.replace(item_to_remove, '')
+        dict_to_remove_from['properties'][field_name] = new_list
+
+        return dict_to_remove_from
 
     def import_certificate_or_key(self):
         #SSL certificates/Licence keys?
@@ -98,16 +153,17 @@ class StingrayDriver(BaseDriver):
 
     def create_real_server(self, rserver):
         #Create node in our terminology
+        #Do we need to do anythign here?
 
         logger.debug("Called DummyStingrayDriver.createRServer(%r).", rserver)
 
     def delete_real_server(self, rserver):
         #Delete node in our terminology
+        #Do we need to do anything here?
 
         logger.debug("Called DummyStingrayDriver.deleteRServer(%r).", rserver)
 
     def activate_real_server(self, serverfarm, rserver):
-        #??
         logger.debug("Called DummyStingrayDriver.activateRServer(%r, %r).",
                      serverfarm, rserver)
 
@@ -116,7 +172,6 @@ class StingrayDriver(BaseDriver):
                      rserver)
 
     def suspend_real_server(self, serverfarm, rserver):
-        #??
         logger.debug("Called DummyStingrayDriver.suspendRServer(%r, %r).",
                      serverfarm, rserver)
 
@@ -131,17 +186,27 @@ class StingrayDriver(BaseDriver):
         logger.debug("Called DummyStingrayDriver.deleteProbe(%r).", probe)
 
     def create_server_farm(self, serverfarm, predictor):
-        ''' Set up virtual server and pool and connect the two
+        ''' Sets up virtual server and pool and connects the two. Virtual server
+        defaultly listens on all addresses, changed to traffic IP group later on.
+        Stingray defaultly throws an error if pool has no nodes, but this dissapears
+        once a node is added.
+
+        ID of serverfarm is used to name node, pool and traffic IP group on stingray.
         '''
         #??Logical naming??
         #Create pool with no attached nodes
         target = 'pools/' + serverfarm['id'] + '/'
-        data = {"properties": {"monitors": "ping"}}
+        pool_new = {"properties": {
+            "monitors": "ping"
+            }
+        }
 
-        self.send_request(target, 'PUT', data)
+        self.send_request(target, 'PUT', pool_new)
 
         #Create Virtual Server for this Load balancing Service
-        self.default_vserver = {"properties": {
+        #Linked to pool by giving the name of pool in the dictionary
+        target = 'vservers/' + serverfarm['id'] + '/'
+        vserver_new = {"properties": {
             "enabled": "no",
             "port": "8080",
             "timeout": 40,
@@ -149,62 +214,62 @@ class StingrayDriver(BaseDriver):
             }
         }
 
-        target = 'vservers/' + serverfarm['id'] + '/'
-        self.send_request(target, 'PUT',
-                              payload=self.default_vserver)
+        self.send_request(target, 'PUT', vserver_new)
 
     def delete_server_farm(self, serverfarm):
-        #Delete pool referenced by serverfarm
+        '''Delete pool, vserver and any traffic IP group referenced by
+        serverfarm.
+        '''
+        #DELETE request to Pool
         target = 'pools/' + serverfarm['id'] + '/'
         self.send_request(target, 'DELETE')
 
-    def add_real_server_to_server_farm(self, serverfarm, rserver):
-        logger.debug('creating')
-        logger.debug(rserver.address + ':' + rserver.port)
-        #Add node to pool
-        target = 'pools/' + serverfarm['id'] + '/'
+        #DELETE request to vserver
+        target = 'vservers/' + serverfarm['id'] + '/'
+        self.send_request(target, 'DELETE')
 
-        #TODO: Get new version of REST so list syntax used.
-        #space seperated variable string for now.
-#ERROR
-        sf_current_json = self.send_request(target, 'GET').text
-        sf_current = json.loads(sf_current_json)
-
+        #DELETE request to traffic IP group
         try:
-            nodelist = sf_current['properties']['nodes']
-        except KeyError:
-            nodelist = ''
-        #Add new node to list and put in correct dictionary form
-#probably
-        newnode = rserver.address + ':' + rserver.port + ' '
-        nodelist =  '"' + nodelist + newnode + '"'
-        data = {"properties": {"nodes": nodelist}}
-#SOMEWHERE HERE
-        self.send_request(target, 'PUT', data)
+            target = 'flipper/' + serverfarm['id'] + '/'
+            response = self.send_request(target, 'DELETE')
+        except HTTPError:
+            #Traffic IP group may not exist, if so just continue
+            if response.status_code == '404':
+                pass
+            else:
+                raise
+
+
+    def add_real_server_to_server_farm(self, serverfarm, rserver):
+        ''' Adds a new node to the nodelist for pool associated with
+        this serverfarm
+        '''
+        #Set up variables for request
+        target = 'pools/' + serverfarm['id'] + '/'
+        new_node = rserver.address + ':' + rserver.port + ' '
+        pool_mod = {"properties": {
+             "nodes": ''
+            }
+        }
+
+        #Modify dictionary and send PUT request with that dictionary
+        pool_mod = self.rest_add_to_list(target, 'nodes', new_node, pool_mod)
+        self.send_request(target, 'PUT', pool_mod)
 
     def delete_real_server_from_server_farm(self, serverfarm, rserver):
-        logger.debug('deleting')
-        #Remove node from pool
-        target = 'pools/' + serverfarm['id'] + '/'
-
-        #TODO: Get new version of REST so list syntax used.
-        #space seperated variable string for now.
-        sf_current_json = self.send_request(target, 'GET').text
-        sf_current = json.loads(sf_current_json)
-        try:
-            nodelist = sf_current['properties']['nodes']
-        except KeyError:
-            #Raise no server found exception?
-            nodelist = ''
-        '''Remove node from list, package existing nodes in
-        correct dictionary form
+        '''Remove node from nodelist for pool associated with this serverfarm
         '''
-        nodelist.replace(rserver.address + ':' + rserver.port + ' ',
-                             '')
-        data = {"properties": {"nodes": nodelist}}
+        #Set up variables for request
+        target = 'pools/' + serverfarm['id'] + '/'
+        node = rserver.address + ':' + rserver.port + ' '
+        pool_mod = {"properties": {
+             "nodes": ''
+            }
+        }
 
+        #Modify dictionary and send PUT request with that dictionary
+        pool_mod = self.rest_delete_from_list(target, 'nodes', node, pool_mod)
         self.send_request(target, 'PUT', data)
-        logger.debug('PUT was from delete_real_server_from_server_farm')
 
     def add_probe_to_server_farm(self, serverfarm, probe):
         logger.debug("Called DummyStingrayDriver.addProbeToSF(%r, %r).",
@@ -223,9 +288,39 @@ class StingrayDriver(BaseDriver):
                      sticky)
 
     def create_virtual_ip(self, vip, serverfarm):
-        #Traffic IP in our terminology
-        logger.debug("Called DummyStingrayDriver.createVIP(%r, %r).",
-                     vip, serverfarm)
+        ''' Add the virtual IP to the traffic IP group associated with this
+        serverfarm. If no traffic IP exists yet then create it with this IP.
+        '''
+        #Add support for masks and ports?
+
+        target = 'flipper/' + serverfarm.id + '/'
+
+        try:
+            #Add IP to existing traffic Ip group
+            traffic_ip_mod = {'properties' : {
+                'ipaddresses': '',
+                }
+            }
+            traffic_ip_mod = self.rest_delete_from_list(target, 'ipaddresses',
+                                            vip.address, traffic_ip_mod)
+
+        except HTTPError:
+            #No traffic IP group exists, create one
+            traffic_ip_new = {'properties' : {
+                'ipaddresses': vip.address,
+                'machines': self.device_name
+                }
+            }
+            self.send_request(target, 'PUT', traffic_ip_new)
+
+            #hook it up to the virtual server
+            vserver_target = 'vservers/' + serverfarm.id + '/'
+            vserver_mod = {'properties': {
+                        'address':('!' + serverfarm.id),
+                }
+            }
+            self.send_request(vserver_target, 'PUT', vserver_mod)
+
 
     def delete_virtual_ip(self, vip):
         #Traffic IP in our terminology
